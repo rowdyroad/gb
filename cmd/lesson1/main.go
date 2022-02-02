9554e7be59e078e61384c395b87b968217ef5578
rowdyroad <b.strelchik@gmail.com>
2022-02-02 23:44:15 +0300

    = fmt

--- cmd/lesson1/main.go
+++ cmd/lesson1/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(mymath.Min(1,2))
-	fmt.Println(mymath.Max(10,1333))
+	fmt.Println(mymath.Min(1, 2))
+	fmt.Println(mymath.Max(10, 1333))
 	fmt.Println(mymath.Abs(-99))
 }
--- cmd/lesson2/calculator.go
+++ cmd/lesson2/calculator.go
@@ -14,26 +14,29 @@ const (
 )
 
 func main() {
-	var a,b float64
+	var a, b float64
 	var operator string
 	fmt.Println("Введите операнд операцию и второй операнд:")
 	fmt.Scan(&a, &operator, &b)
 	var res float64
 	switch operator {
-		case 	OperationAdd:
-			res = a + b
-		case	OperationSub:
-			res = a - b
-		case	OperationMul:
-			res = a * b
-		case	OperationDev:
-			if b == 0 {
-				fmt.Println("Ошибка! Деление на ноль")
-				return
-			}
-			res = a / b
-		case	OperationPow:
-			res = math.Pow(a,b)
+	case OperationAdd:
+		res = a + b
+	case OperationSub:
+		res = a - b
+	case OperationMul:
+		res = a * b
+	case OperationDev:
+		if b == 0 {
+			fmt.Println("Ошибка! Деление на ноль")
+			return
+		}
+		res = a / b
+	case OperationPow:
+		res = math.Pow(a, b)
+	default:
+		fmt.Println("Ошибка! Неизвестная операция")
+		return
 	}
 
 	fmt.Println("Результат: ", res)
